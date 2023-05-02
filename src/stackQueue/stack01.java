e3e44d9f708a1a9e5bf599eb6457a1a9b9f603ff
seoyoung <seo08905@gmail.com>
2023-05-02 21:36:06 +0900

    stack

--- src/stackQueue/stack01.java
+++ src/stackQueue/stack01.java
@@ -1,8 +1,34 @@
 package stackQueue;
 
+import java.util.Stack;
+
 public class stack01 {
 
     public static void main(String[] args) {
+//        String s = "())())";
+        String s = "((()))";
+        System.out.println(solution(s));
+    }
 
+    static boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i= 0 ; i< s.length() ; i++ ) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                st.push(ch);
+            }
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        if (!st.empty()) {
+            return false;
+        }
+        return true;
     }
 }
